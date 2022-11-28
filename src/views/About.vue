3db74b4cae4fa8c5f0809c9331cddbf9f9a5a302
Evanson Biwott <509332-evansonbiwott@users.noreply.gitlab.com>
2022-11-28 12:07:27 +0300

    Add routes and views
    - Update README.md

---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="about">
+        <h3>Version 1.0.0</h3>
+        <router-link to="/">Go Back</router-link>
+    </div>
+    
+</template>
+
+<style>
+    .about {
+        text-align: center;
+    }
+</style>
